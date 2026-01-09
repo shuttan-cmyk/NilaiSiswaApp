067ae0c2fce391999a663ee699273800913e021d
shuttan-cmyk <shuttanpramesti@gmail.com>
2026-01-09 14:29:54 +0700

    penambahan input nama dan nilai

--- NilaiSiswaApp/Program.cs
+++ NilaiSiswaApp/Program.cs
@@ -2,3 +2,9 @@
 string nama;
 
 Console.Write(" Aplikasi Nilai Siswa");
+
+Console.Write("nama siswa");
+nama = Console.ReadLine();
+
+Console.Write("nilai: ");
+nilai = int.Parse(Console.ReadLine());
